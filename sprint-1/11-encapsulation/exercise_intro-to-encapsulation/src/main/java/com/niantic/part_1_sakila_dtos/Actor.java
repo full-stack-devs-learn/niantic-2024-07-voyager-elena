8281a36c26768f29b0dc3202e34242977b182a28
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-05 13:31:48 -0700

    Day11: Create the Actor DTO class

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
@@ -1,5 +1,47 @@
 package com.niantic.part_1_sakila_dtos;
 
-public class Actor
-{
+//import java.util.Date;
+
+public class Actor {
+    private int actorId;
+    private String firstName;
+    private String lastName;
+    // private Date lastUpdate;
+
+    public Actor() {
+    }
+
+    public Actor(int actorId, String firstName, String lastName) {
+        this.actorId = actorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(int actorId) {
+        this.actorId = actorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
 }
