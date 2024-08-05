515268b76da161cdcf0698c7fad08c1abf446b82
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-05 14:16:30 -0700

    Day11: Create the MathTest class

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_2_objects/MathTest.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_2_objects/MathTest.java
@@ -1,5 +1,53 @@
 package com.niantic.part_2_objects;
 
-public class MathTest
-{
+public class MathTest {
+
+    private final int POSSIBLE_POINTS;
+    private final String STUDENT_NAME;
+    private int score;
+
+
+    public MathTest(int possiblePoints, String studentName) {
+        this.POSSIBLE_POINTS = possiblePoints;
+        this.STUDENT_NAME = studentName;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getPossiblePoints() {
+        return POSSIBLE_POINTS;
+    }
+
+    public String getStudentName() {
+        return STUDENT_NAME;
+    }
+
+    public String getLetterGrade() {
+        int percent = getPercent();
+
+        if (percent >= 90) {
+            return "A";
+        }
+        if (percent >= 80) {
+            return "B";
+        }
+        if (percent >= 70) {
+            return "C";
+        }
+        if (percent >= 60) {
+            return "D";
+        }
+        return "F";
+    }
+
+    public int getPercent() {
+        return (int) Math.round(score * 100.00 / POSSIBLE_POINTS);
+    }
+
 }
