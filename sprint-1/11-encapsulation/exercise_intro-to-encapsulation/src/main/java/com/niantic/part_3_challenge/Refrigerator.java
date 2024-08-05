d155396093fc938830a5f194c8e9f3f512aa0c6a
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-05 14:56:15 -0700

    Day11: Create the Refrigerator class

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Refrigerator.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Refrigerator.java
@@ -1,5 +1,54 @@
 package com.niantic.part_3_challenge;
 
-public class Refrigerator
-{
+public class Refrigerator {
+    private final int CURRENT_TEMPERATURE;
+    private final int MAX_CAPACITY;
+    private int capacityAvailable;
+    private boolean isDoorOpen;
+
+    public Refrigerator(int currentTemperature, int maxCapacity) {
+        this.CURRENT_TEMPERATURE = currentTemperature;
+        this.MAX_CAPACITY = maxCapacity;
+        capacityAvailable = MAX_CAPACITY;
+    }
+
+    public int getCurrentTemperature() {
+        return CURRENT_TEMPERATURE;
+    }
+
+    public int getMaxCapacity() {
+        return MAX_CAPACITY;
+    }
+
+    public int getAvailableCapacity() {
+        return capacityAvailable;
+    }
+
+    public boolean isDoorOpen() {
+        return isDoorOpen;
+    }
+
+    public void openDoor() {
+        isDoorOpen = true;
+    }
+
+    public void closeDoor() {
+        isDoorOpen = false;
+    }
+
+    public boolean addItem(int capacity) {
+        if (isDoorOpen && capacityAvailable >= capacity) {
+            capacityAvailable -= capacity;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeItem(int capacity) {
+        if (isDoorOpen && capacity <= capacityAvailable) {
+            capacityAvailable += capacity;
+            return true;
+        }
+        return false;
+    }
 }
