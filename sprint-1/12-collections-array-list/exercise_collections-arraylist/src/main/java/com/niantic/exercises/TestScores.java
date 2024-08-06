c0d0e7bc0bd9ad5e7d5dc7afee819b7b7d92a0cf
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-06 14:20:29 -0700

    Day11: complete all the TestScores exercises

--- sprint-1/12-collections-array-list/exercise_collections-arraylist/src/main/java/com/niantic/exercises/TestScores.java
+++ sprint-1/12-collections-array-list/exercise_collections-arraylist/src/main/java/com/niantic/exercises/TestScores.java
@@ -14,7 +14,15 @@ public class TestScores
      */
     public ArrayList<TestScore> getScoresByTest(ArrayList<TestScore> testScores, String testName)
     {
-        return null;
+        ArrayList<TestScore> filteredTestScores = new ArrayList<>();
+
+        for (var testScore : testScores) {
+            if (testScore.getTestName().equals(testName)) {
+                filteredTestScores.add(testScore);
+            }
+        }
+
+        return filteredTestScores;
     }
 
     /*
@@ -22,89 +30,124 @@ public class TestScores
         Given an input of All testScores and the name of a student - return all scores for the
         requested student.
      */
-    public ArrayList<TestScore> getScoresByStudent(ArrayList<TestScore> testScores, String student)
-    {
-        return null;
+    public ArrayList<TestScore> getScoresByStudent(ArrayList<TestScore> testScores, String student) {
+        ArrayList<TestScore> filteredTestScores = new ArrayList<>();
+
+        for (var testScore : testScores) {
+            if (testScore.getStudentName().equals(student)) {
+                filteredTestScores.add(testScore);
+            }
+        }
+
+        return filteredTestScores;
     }
 
     /*
     3.  An ArrayList of TestScores contains test results for all students and all of their tests
         Given an input of All testScores return the highest score.
      */
-    public int getHighestScore(ArrayList<TestScore> testScores)
-    {
-        return 0;
+    public int getHighestScore(ArrayList<TestScore> testScores) {
+        int highestScore = 0; // I believe that test scores cannot be negative
+
+        for (var testScore : testScores) {
+            highestScore = Math.max(highestScore, testScore.getScore());
+        }
+
+        return highestScore;
     }
 
     /*
     4.  An ArrayList of TestScores contains test results for all students and all of their tests
         Given an input of All testScores return the lowest score.
      */
-    public int getLowestScore(ArrayList<TestScore> testScores)
-    {
-        return 0;
+    public int getLowestScore(ArrayList<TestScore> testScores) {
+        int lowestScore = testScores.getFirst().getScore();
+
+        // which way is more correct? get first value from list or initialize with max value possible?
+        // int lowestScore = Integer.MAX_VALUE;
+
+        for (var testScore : testScores) {
+            lowestScore = Math.min(lowestScore, testScore.getScore());
+        }
+
+        return lowestScore;
     }
 
     /*
     5.  An ArrayList of TestScores contains test results for all students and all of their tests
         Given an input of All testScores return the lowest score.
      */
-    public int getAverageScore(ArrayList<TestScore> testScores)
-    {
-        return 0;
+    public int getAverageScore(ArrayList<TestScore> testScores) {
+        int average = 0; // int???
+
+        for (var testScore : testScores) {
+            average += testScore.getScore();
+        }
+        average /= testScores.size();
+
+        return average;
     }
 
     /*
     6.  An ArrayList of TestScores contains test results for all students and all of their tests
         Given an input of All testScores return the highest score for the specified test name.
      */
-    public int getHighestScoreByTest(ArrayList<TestScore> testScores, String testName)
-    {
-        return 0;
+    public int getHighestScoreByTest(ArrayList<TestScore> testScores, String testName) {
+        // the easiest way to do it is to use functions we already have
+        // but maybe it is not the most efficient way because we loop twice,
+        // anyway it is O(n), where n = testScores.size()
+
+        // return getHighestScore(getScoresByTest(testScores, testName));
+
+        // another way to solve it with one loop
+        int highestScore = 0;
+
+        for (var testScore : testScores) {
+            if (testScore.getTestName().equals(testName)) {
+                highestScore = Math.max(highestScore, testScore.getScore());
+            }
+        }
+
+        return highestScore;
     }
 
     /*
     7.  An ArrayList of TestScores contains test results for all students and all of their tests
         Given an input of All testScores return the lowest score for the specified test name.
      */
-    public int getLowestScoreByTest(ArrayList<TestScore> testScores, String testName)
-    {
-        return 0;
+    public int getLowestScoreByTest(ArrayList<TestScore> testScores, String testName) {
+        return getLowestScore(getScoresByTest(testScores, testName));
     }
 
     /*
     8.  An ArrayList of TestScores contains test results for all students and all of their tests
         Given an input of All testScores return the average score for the specified test name.
      */
-    public int getAverageScoreByTest(ArrayList<TestScore> testScores, String testName)
-    {
-        return 0;
+    public int getAverageScoreByTest(ArrayList<TestScore> testScores, String testName) {
+        return getAverageScore(getScoresByTest(testScores, testName));
     }
 
     /*
     9.  An ArrayList of TestScores contains test results for all students and all of their tests
         Given an input of All testScores return the highest score for the specified student.
      */
-    public int getHighestScoreByStudent(ArrayList<TestScore> testScores, String student)
-    {
-        return 0;
+    public int getHighestScoreByStudent(ArrayList<TestScore> testScores, String student) {
+        return getHighestScore(getScoresByStudent(testScores, student));
     }
 
     /*
     10.  An ArrayList of TestScores contains test results for all students and all of their tests
         Given an input of All testScores return the lowest score for the specified student.
      */
-    public int getLowestScoreByStudent(ArrayList<TestScore> testScores, String student)
-    {
-        return 0;
+    public int getLowestScoreByStudent(ArrayList<TestScore> testScores, String student) {
+        return getLowestScore(getScoresByStudent(testScores, student));
     }
 
     /*
     11.  An ArrayList of TestScores contains test results for all students and all of their tests
         Given an input of All testScores return the average score for the specified student.
      */
-    public int getAverageScoreByStudent(ArrayList<TestScore> testScores, String student)
-    {
-        return 0;
+    public int getAverageScoreByStudent(ArrayList<TestScore> testScores, String student) {
+        return getAverageScore(getScoresByStudent(testScores, student));
     }
 }
