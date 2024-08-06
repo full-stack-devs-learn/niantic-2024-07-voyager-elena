639fea70192f66a6dd016de54ad1296e509e6aa5
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-06 13:40:47 -0700

    Day12: complete all the ArrayListIntro exercices

--- sprint-1/12-collections-array-list/exercise_collections-arraylist/src/main/java/com/niantic/exercises/ArrayListIntro.java
+++ sprint-1/12-collections-array-list/exercise_collections-arraylist/src/main/java/com/niantic/exercises/ArrayListIntro.java
@@ -1,18 +1,30 @@
 package com.niantic.exercises;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class ArrayListIntro
-{
+public class ArrayListIntro {
     /*
     1. Create an ArrayList of Strings and add the names of your
        5 favorite heroes.
 
     Return the list.
      */
-    public ArrayList<String> getHeroesList()
-    {
-        return null;
+    public ArrayList<String> getHeroesList() {
+
+//        ArrayList<String> myFavoriteHeroes = new ArrayList<>(5);
+//
+//        myFavoriteHeroes.add("Wolverine");
+//        myFavoriteHeroes.add("Superman");
+//        myFavoriteHeroes.add("Captain America");
+//        myFavoriteHeroes.add("Black Widow");
+//        myFavoriteHeroes.add("Mystique");
+
+
+        // another way to do it
+        ArrayList<String> myFavoriteHeroes = new ArrayList<>(Arrays.asList(new String[]{"Wolverine", "Superman", "Captain America", "Black Widow", "Mystique"}));
+
+        return myFavoriteHeroes;
     }
 
     /*
@@ -22,9 +34,16 @@ public class ArrayListIntro
        findEvens( [1, 2, 3, 4] )            ->  [2, 4]
        findEvens( [21, 98, 78, 5, 6, 8] )   ->  [98, 78, 6, 8]
      */
-    public ArrayList<Integer> findEvens(ArrayList<Integer> numbers)
-    {
-        return null;
+    public ArrayList<Integer> findEvens(ArrayList<Integer> numbers) {
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
+
+        for (var num : numbers) {
+            if (num % 2 == 0) {
+                evenNumbers.add(num);
+            }
+        }
+
+        return evenNumbers;
     }
 
     /*
@@ -33,9 +52,14 @@ public class ArrayListIntro
        sum( [1, 2, 3, 4] )            ->  10
        sum( [21, 98, 78, 5, 6, 8] )   ->  216
      */
-    public int sum(ArrayList<Integer> numbers)
-    {
-        return 0;
+    public int sum(ArrayList<Integer> numbers) {
+        int sum = 0;
+
+        for (var num : numbers) {
+            sum += num;
+        }
+
+        return sum;
     }
 
     /*
@@ -44,9 +68,14 @@ public class ArrayListIntro
        sum( [1, 2, 3, 4] )            ->  4
        sum( [21, 98, 78, 5, 6, 8] )   ->  98
      */
-    public int max(ArrayList<Integer> numbers)
-    {
-        return 0;
+    public int max(ArrayList<Integer> numbers) {
+        int maxNumber = Integer.MIN_VALUE;
+
+        for (var num : numbers) {
+            maxNumber = Math.max(maxNumber, num);
+        }
+
+        return maxNumber;
     }
 
     /*
@@ -55,9 +84,14 @@ public class ArrayListIntro
        sum( [1, 2, 3, 4] )            ->  1
        sum( [21, 98, -78, 5, 6, 8] )  ->  -78
      */
-    public int min(ArrayList<Integer> numbers)
-    {
-        return 0;
+    public int min(ArrayList<Integer> numbers) {
+        int minNumber = Integer.MAX_VALUE;
+
+        for (var num : numbers) {
+            minNumber = Math.min(minNumber, num);
+        }
+
+        return minNumber;
     }
 
     /*
@@ -67,9 +101,9 @@ public class ArrayListIntro
        sum( [3, 1, 59, -4, 81, 23] )    ->  27
        sum( [21, 98, -78, 5, 6, 8] )    ->  53
      */
-    public int average(ArrayList<Integer> numbers)
-    {
-        return 0;
+    public int average(ArrayList<Integer> numbers) {
+        // what's a little bit strange that we need to return integer here
+        return sum(numbers) / numbers.size();
     }
 
     /*
@@ -86,8 +120,16 @@ public class ArrayListIntro
 
         Include as many numbers as is specified by the size input
      */
-    public ArrayList<Integer> buildFibonacci(int size)
-    {
-        return null;
+    public ArrayList<Integer> buildFibonacci(int size) {
+        ArrayList<Integer> fibonacciiList = new ArrayList<>(size);
+
+        fibonacciiList.add(0);
+        fibonacciiList.add(1);
+
+        for (int i = 2; i < size; i++) {
+            fibonacciiList.add(fibonacciiList.get(i - 2) + fibonacciiList.get(i - 1));
+        }
+
+        return fibonacciiList;
     }
 }
