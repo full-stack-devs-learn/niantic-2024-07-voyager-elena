dac8e5ae6148cd7981e7593d7a0d282efe2f278a
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-07 15:00:22 -0700

    Day13: complete the CategoryDao class

--- sprint-1/13-data-access-objects/exercise_mapping-data-to-classes/src/main/java/com/niantic/services/CategoryDao.java
+++ sprint-1/13-data-access-objects/exercise_mapping-data-to-classes/src/main/java/com/niantic/services/CategoryDao.java
@@ -65,6 +65,25 @@ public class CategoryDao
      */
     public Category getCategoryById(int categoryId)
     {
+        String sql = """
+                SELECT category_id
+                    , category_name
+                    , description
+                FROM categories
+                WHERE category_id = ?;
+                """;
+
+        var row = jdbcTemplate.queryForRowSet(sql, categoryId);
+
+        if(row.next())
+        {
+            return new Category(
+                    row.getInt("category_id"),
+                    row.getString("category_name"),
+                    row.getString("description")
+            );
+        }
+
         return null;
     }
 
@@ -75,6 +94,11 @@ public class CategoryDao
      */
     public void addCategory(Category category)
     {
+        String sql = "INSERT INTO categories (category_name, description) VALUES (?,?);";
+
+        jdbcTemplate.update(sql,
+                category.getCategoryName(),
+                category.getDescription());
     }
 
     /*
@@ -84,6 +108,18 @@ public class CategoryDao
      */
     public void updateCategory(Category category)
     {
+        String sql = """
+                UPDATE categories
+                SET category_name = ?
+                	, description = ?
+                WHERE category_id = ?;
+                """;
+
+        jdbcTemplate.update(sql,
+                category.getCategoryName(),
+                category.getDescription(),
+                category.getCategoryId()
+        );
     }
 
     /*
@@ -92,7 +128,9 @@ public class CategoryDao
      */
     public void deleteCategory(int categoryId)
     {
-    }
+        String sql = "DELETE FROM categories WHERE category_id = ?;";
 
+        jdbcTemplate.update(sql, categoryId);
+    }
 
 }
