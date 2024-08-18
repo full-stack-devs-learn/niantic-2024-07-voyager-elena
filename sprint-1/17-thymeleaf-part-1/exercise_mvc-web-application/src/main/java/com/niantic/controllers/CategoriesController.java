b175eea09fd4ddba70a5a5b5c7e12617ea347682
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-18 15:29:51 -0700

    Day18: Spring Boot+Thymeleaf-Part2 - add category

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
@@ -5,7 +5,9 @@ import com.niantic.services.CategoryDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 
@@ -25,6 +27,52 @@ public class CategoriesController {
         var category = categoryDao.getCategoryById(id);
         model.addAttribute("category", category);
         return "categories/details";
+    }
+
+    @GetMapping("/categories/add")
+    public String addCategories(Model model) {
+        model.addAttribute("category", new Category());
+        model.addAttribute("action", "add");
+        return "categories/add_edit";
+    }
+
+    @PostMapping("/categories/add")
+    public String addCategory(Model model, @ModelAttribute("category") Category category) {
+        int categoryId = categoryDao.addCategory(category);
+        category.setCategoryId(categoryId);
+        model.addAttribute("category", category);
+        return "categories/add_success";
+    }
+
+    @GetMapping("/categories/{id}/edit")
+    public String editCategory(Model model, @PathVariable int id) {
+        Category category = categoryDao.getCategoryById(id);
+        model.addAttribute("category", category);
+        model.addAttribute("action", "edit");
+        return "categories/add_edit";
+    }
+
+    @PostMapping("/categories/{id}/edit")
+    public String editCategory(Model model, @ModelAttribute("categories") Category category, @PathVariable int id) {
+        Category prevCategory = categoryDao.getCategoryById(id);
+        category.setCategoryId(id);
+        categoryDao.updateCategory(category);
+        model.addAttribute("prevCategory", prevCategory);
+        model.addAttribute("category", category);
+        return "redirect:/edit_success";
+    }
+
+    @GetMapping("/categories/{id}/delete")
+    public String deleteCategory(Model model, @PathVariable int id) {
+        Category category = categoryDao.getCategoryById(id);
+        model.addAttribute("category", category);
+        return "categories/delete";
+    }
+
+    @PostMapping("/categories/{id}/delete")
+    public String deleteCategory(@PathVariable int id) {
+        categoryDao.deleteCategory(id);
+        return "redirect:/categories";
     }
 
 }
--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/services/CategoryDao.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/services/CategoryDao.java
@@ -3,21 +3,23 @@ package com.niantic.services;
 import com.niantic.models.Category;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.ArrayList;
 
-public class CategoryDao
-{
+public class CategoryDao {
     private final JdbcTemplate jdbcTemplate;
 
-    public CategoryDao()
-    {
+    public CategoryDao() {
         String databaseUrl = "jdbc:mysql://localhost:3306/northwind";
         String userName = "root";
         String password = "P@ssw0rd";
-        DataSource dataSource = new BasicDataSource(){{
+        DataSource dataSource = new BasicDataSource() {{
             setUrl(databaseUrl);
             setUsername(userName);
             setPassword(password);
@@ -26,19 +28,17 @@ public class CategoryDao
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public ArrayList<Category> getCategories()
-    {
+    public ArrayList<Category> getCategories() {
         ArrayList<Category> categories = new ArrayList<>();
 
         String sql = """
-        SELECT category_id, category_name, description
-        FROM categories
-        """;
+                SELECT category_id, category_name, description
+                FROM categories
+                """;
 
         SqlRowSet row = jdbcTemplate.queryForRowSet(sql);
 
-        while(row.next())
-        {
+        while (row.next()) {
             int categoryId = row.getInt("category_id");
             String categoryName = row.getString("category_name");
             String description = row.getString("description");
@@ -51,20 +51,18 @@ public class CategoryDao
         return categories;
     }
 
-    public Category getCategoryById(int categoryId)
-    {
+    public Category getCategoryById(int categoryId) {
         Category category = null;
 
         String sql = """
-                SELECT category_id, category_name, description
-                FROM categories
-                WHERE category_id = ?
-        """;
+                        SELECT category_id, category_name, description
+                        FROM categories
+                        WHERE category_id = ?
+                """;
 
         SqlRowSet row = jdbcTemplate.queryForRowSet(sql, categoryId);
 
-        if(row.next())
-        {
+        if (row.next()) {
             String categoryName = row.getString("category_name");
             String description = row.getString("description");
 
@@ -75,21 +73,38 @@ public class CategoryDao
         return category;
     }
 
-    public void addCategory(Category category)
-    {
+    public int addCategory(Category category) {
         String sql = """
                 INSERT INTO categories (category_name, description)
                 VALUES (?,?)
                 """;
 
-        jdbcTemplate.update(sql
-                , category.getCategoryName()
-                , category.getDescription());
+//        jdbcTemplate.update(sql
+//                , category.getCategoryName()
+//                , category.getDescription());
+
+        // to get id of just added category
+        // found this example here - https://www.springcloud.io/post/2022-06/jdbctemplate-id
+        GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(conn -> {
+
+            // Pre-compiling SQL
+            PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            // Set parameters
+            preparedStatement.setString(1, category.getCategoryName());
+            preparedStatement.setString(2, category.getDescription());
+
+            return preparedStatement;
+
+        }, generatedKeyHolder);
+
+        return generatedKeyHolder.getKey().intValue();
+
     }
 
 
-    public void updateCategory(Category category)
-    {
+    public void updateCategory(Category category) {
         String sql = """
                 UPDATE categories
                 SET category_name = ?
@@ -104,8 +119,7 @@ public class CategoryDao
     }
 
 
-    public void deleteCategory(int categoryId)
-    {
+    public void deleteCategory(int categoryId) {
         String sql = """
                 DELETE FROM categories
                 WHERE category_id = ?
