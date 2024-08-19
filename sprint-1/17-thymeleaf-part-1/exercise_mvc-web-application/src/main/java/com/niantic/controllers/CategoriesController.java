883abd05495becc5cab8d4cc13927ecbb0eb79e5
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-18 19:00:31 -0700

    Day18: Spring Boot+Thymeleaf-Part2 - add error.html template
    -add error template to show users nice error page instead of app crashing
    for example when category 's name is longer that 15 characters and the category  with such a long name cannot be added/updated

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
@@ -38,10 +38,14 @@ public class CategoriesController {
 
     @PostMapping("/categories/add")
     public String addCategory(Model model, @ModelAttribute("category") Category category) {
-        int categoryId = categoryDao.addCategory(category);
-        category.setCategoryId(categoryId);
-        model.addAttribute("category", category);
-        return "categories/add_success";
+        try {
+            int categoryId = categoryDao.addCategory(category);
+            category.setCategoryId(categoryId);
+            model.addAttribute("category", category);
+            return "categories/add_success";
+        } catch (Exception e) {
+            return ("error");
+        }
     }
 
     @GetMapping("/categories/{id}/edit")
@@ -54,12 +58,16 @@ public class CategoriesController {
 
     @PostMapping("/categories/{id}/edit")
     public String editCategory(Model model, @ModelAttribute("categories") Category category, @PathVariable int id) {
-        Category prevCategory = categoryDao.getCategoryById(id);
-        category.setCategoryId(id);
-        categoryDao.updateCategory(category);
-        model.addAttribute("prevCategory", prevCategory);
-        model.addAttribute("category", category);
-        return "categories/edit_success";
+        try {
+            Category prevCategory = categoryDao.getCategoryById(id);
+            category.setCategoryId(id);
+            categoryDao.updateCategory(category);
+            model.addAttribute("prevCategory", prevCategory);
+            model.addAttribute("category", category);
+            return "categories/edit_success";
+        } catch (Exception e) {
+            return ("error");
+        }
     }
 
     @GetMapping("/categories/{id}/delete")
@@ -71,10 +79,14 @@ public class CategoriesController {
 
     @PostMapping("/categories/{id}/delete")
     public String deleteCategory(Model model, @PathVariable int id) {
-        Category category = categoryDao.getCategoryById(id);
-        model.addAttribute("category", category);
-        categoryDao.deleteCategory(id);
-        return "categories/delete_success";
+        try {
+            Category category = categoryDao.getCategoryById(id);
+            model.addAttribute("category", category);
+            categoryDao.deleteCategory(id);
+            return "categories/delete_success";
+        } catch (Exception e) {
+            return ("error");
+        }
     }
 
 }
