bc73d96f49b1dea13bc52b8d8385fe8080bcdd03
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-20 13:25:34 -0700

    Day22: Inheritance - create the Knight class

--- sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Knight.java
+++ sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Knight.java
@@ -1,5 +1,46 @@
 package com.niantic;
 
-public class Knight
-{
+public class Knight extends Character {
+
+    private int armor;
+
+    public Knight(String name, int health, int level, int experience, int armor) {
+        super(name, health, level, experience);
+        this.armor = armor;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        // The armor should deflect the amount of damage that the armor can handle
+        // If the armor is more powerful than the attack, then no damage is sustained by the knight
+        // Any additional damage should be removed from the health of the knight.
+        if (armor > 0) {
+            System.out.println("The damage was decreased because " + name + " has special ability: " + specialAbility() + " and the armor = " + armor);
+            damage -= armor;
+            if (damage < 0) {
+                damage = 0;
+            }
+        }
+        super.takeDamage(damage);
+
+        // The armor's level does not decrease, but can be re-used
+        // so no need to change armor
+    }
+
+    @Override
+    public void levelUp() {
+        // should perform all of the levelUp functions of a character levelUp()
+        super.levelUp();
+        // should also increase the armor by 5 points
+        armor += 5;
+    }
+
+    @Override
+    public String specialAbility() {
+        return "Armor Shield";
+    }
 }
