62337795cc5ee2020d1c9774cf13299078bf0f10
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-21 14:22:25 -0700

    Day22: Inheritance - refactor tests for the Knight class
    - move all Knight methods into setup()
    - call all Knight method using Reflections

--- sprint-2/02-inheritance/exercise_inheritance/src/test/java/com/niantic/KnightTest.java
+++ sprint-2/02-inheritance/exercise_inheritance/src/test/java/com/niantic/KnightTest.java
@@ -6,22 +6,36 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class KnightTest extends ReflectionBase<Knight> {
     Class<Knight> knightClass;
+    Constructor constructor;
+    Method getArmor;
+    Method attack;
+    Method levelUp;
+    Method getHealth;
+    Method getLevel;
 
     @BeforeEach
     void setup() {
         knightClass = Knight.class;
+        constructor = getConstructor(knightClass, String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE, Integer.TYPE);
+        getArmor = getMethod(knightClass, "getArmor");
+        attack = getMethod(knightClass, "attack", Character.class);
+        levelUp = getMethod(knightClass, "levelUp");
+        getHealth = getMethod(knightClass, "getHealth");
+        getLevel = getMethod(knightClass, "getLevel");
+
     }
 
     @Test
     public void knight_should_extendCharacter() throws InvocationTargetException, InstantiationException, IllegalAccessException {
         // arrange
-        var constructor = getConstructor(knightClass, String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE, Integer.TYPE);
         var knightArthur = constructor.newInstance("Arthur", 50, 1, 100, 5);
 
         // act
@@ -32,7 +46,6 @@ public class KnightTest extends ReflectionBase<Knight> {
     public void knight_shouldHave_correctConstructor() {
         // arrange
         // act
-        var constructor = getConstructor(knightClass, String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE, Integer.TYPE);
 
         // assert
         assertNotNull(constructor, "\nKnight should have a public constructor with (String name, int health, int level, int experience, int armor)");
@@ -42,10 +55,9 @@ public class KnightTest extends ReflectionBase<Knight> {
     public void knight_shouldHave_getArmor() {
         // arrange
         // act
-        var method = getMethod(knightClass, "getArmor");
 
         // assert
-        assertNotNull(method, "\nKnight should have a getArmor() getter method");
+        assertNotNull(getArmor, "\nKnight should have a getArmor() getter method");
     }
 
     @Test
@@ -61,11 +73,7 @@ public class KnightTest extends ReflectionBase<Knight> {
     @Test
     public void attack_should_useBaseCharacterLogic() throws InvocationTargetException, InstantiationException, IllegalAccessException {
         // arrange
-        var constructor = getConstructor(knightClass, String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE, Integer.TYPE);
         var knightArthur = constructor.newInstance("Arthur", 50, 1, 100, 5);
-        var getArmor = getMethod(knightClass, "getArmor");
-        var attack = getMethod(knightClass, "attack", Character.class);
-
         var enemy = new Character("Villain", 50, 1, 100);
 
         // act
@@ -81,26 +89,19 @@ public class KnightTest extends ReflectionBase<Knight> {
     @Test
     public void levelUp_should_increase_healthLevelAndArmor() throws InvocationTargetException, InstantiationException, IllegalAccessException {
         // arrange
-        var constructor = getConstructor(knightClass, String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE, Integer.TYPE);
         var knightArthur = constructor.newInstance("Arthur", 50, 1, 100, 5);
-        var getArmor = getMethod(knightClass, "getArmor");
 
         // act
-        knightArthur.levelUp();
+        levelUp.invoke(knightArthur);
 
         // assert
-        Object armor = getArmor.invoke(knightArthur);
-
         var expectedHealth = 50 + 10;
         var expectedLevel = 2;
         var expectedArmor = 10; // 5 + 5
 
-        var actualHealth = knightArthur.getHealth();
-        var actualLevel = knightArthur.getLevel();
-        var actualArmor = (int) armor;
-        // why we cannot do
-        // var actualArmor = knightArthur.getArmor();
-
+        var actualHealth = getHealth.invoke(knightArthur);
+        var actualLevel = getLevel.invoke(knightArthur);
+        var actualArmor = (int) getArmor.invoke(knightArthur);
 
         assertEquals(expectedHealth, actualHealth, "Because the knight leveled up which should have added health.");
         assertEquals(expectedLevel, actualLevel, "Because the knight leveled up.");
@@ -110,24 +111,19 @@ public class KnightTest extends ReflectionBase<Knight> {
     @Test
     public void levelUp_shouldNot_increase_healthLevelOrArmor_forDefeatedWizard() throws InvocationTargetException, InstantiationException, IllegalAccessException {
         // arrange
-        var constructor = getConstructor(knightClass, String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE, Integer.TYPE);
         var knightArthur = constructor.newInstance("Arthur", 0, 1, 100, 5);
-        var getArmor = getMethod(knightClass, "getArmor");
 
         // act
-        knightArthur.levelUp();
+        levelUp.invoke(knightArthur);
 
         // assert
-        Object armor = getArmor.invoke(knightArthur);
-
         var expectedHealth = 0;
         var expectedLevel = 1;
         var expectedArmor = 5;
 
-        var actualHealth = knightArthur.getHealth();
-        var actualLevel = knightArthur.getLevel();
-        var actualArmor = (int) armor;
-
+        var actualHealth = getHealth.invoke(knightArthur);
+        var actualLevel = getLevel.invoke(knightArthur);
+        var actualArmor = (int) getArmor.invoke(knightArthur);
 
         assertEquals(expectedHealth, actualHealth, "Because defeated knight should NOT have leveled up.");
         assertEquals(expectedLevel, actualLevel, "Because defeated knight should NOT have leveled up.");
@@ -137,9 +133,7 @@ public class KnightTest extends ReflectionBase<Knight> {
     @Test
     public void knight_shouldHave_SpecialAbilityOverridden() throws InvocationTargetException, InstantiationException, IllegalAccessException {
         // arrange
-        var constructor = getConstructor(knightClass, String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE, Integer.TYPE);
         var knightArthur = constructor.newInstance("Arthur", 50, 1, 100, 5);
-        var getArmor = getMethod(knightClass, "getArmor");
         String expectedSpecialAbility = "Armor Shield";
 
         // act
@@ -153,17 +147,14 @@ public class KnightTest extends ReflectionBase<Knight> {
     @Test
     public void takeDamage_shouldTakeArmorInAccount_whenDamageIsMoreThanArmor() throws InvocationTargetException, InstantiationException, IllegalAccessException {
         // arrange
-        var constructor = getConstructor(knightClass, String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE, Integer.TYPE);
         var knightArthur = constructor.newInstance("Arthur", 50, 1, 100, 5);
-        var getArmor = getMethod(knightClass, "getArmor");
-
         var enemy = new Character("Villain", 50, 1, 100);
 
         //act
-        enemy.attack(knightArthur);
+        enemy.attack((Character) knightArthur);
 
         int expectedHealth = 45; // (50 - (10 - 5));
-        int actualHealth = knightArthur.getHealth();
+        int actualHealth = (int) getHealth.invoke(knightArthur);
 
         // assert
         assertEquals(expectedHealth, actualHealth, "The Knight's armor should reduce damage gotten by " + (int) getArmor.invoke(knightArthur));
@@ -172,16 +163,14 @@ public class KnightTest extends ReflectionBase<Knight> {
     @Test
     public void takeDamage_shouldNot_changeArmor() throws InvocationTargetException, InstantiationException, IllegalAccessException {
         // arrange
-        var constructor = getConstructor(knightClass, String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE, Integer.TYPE);
         var knightArthur = constructor.newInstance("Arthur", 50, 1, 100, 5);
-        var getArmor = getMethod(knightClass, "getArmor");
         int armorBefore = (int) getArmor.invoke(knightArthur);
         int expectedArmor = armorBefore;
 
         var enemy = new Character("Villain", 50, 1, 100);
 
         //act
-        enemy.attack(knightArthur);
+        enemy.attack((Character) knightArthur);
         int actualArmor = (int) getArmor.invoke(knightArthur);
 
         // assert
@@ -202,18 +191,15 @@ public class KnightTest extends ReflectionBase<Knight> {
     @ValueSource(ints = {0, 1, 5, 10, 15}) // found example with ValueSource here https://www.baeldung.com/parameterized-tests-junit-5
     public void takeDamage_shouldTakeArmorInAccount(int armor) throws InvocationTargetException, InstantiationException, IllegalAccessException {
         // arrange
-        var constructor = getConstructor(knightClass, String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE, Integer.TYPE);
         var knightArthur = constructor.newInstance("Arthur", 50, 1, 100, armor);
-        var getArmor = getMethod(knightClass, "getArmor");
-
         var enemy = new Character("Villain", 50, 1, 100);
 
         //act
-        enemy.attack(knightArthur);
+        enemy.attack((Character) knightArthur);
 
         int expectedHealth = 50 - Math.max((10 - armor), 0);    // attackDamage = 10, but I cannot get this value here,
-                                                                // it is protected so  I have to hardcode this value
-        int actualHealth = knightArthur.getHealth();
+                                                                // it is protected so I have to hardcode this value
+        int actualHealth = (int) getHealth.invoke(knightArthur);
 
         // assert
         assertEquals(expectedHealth, actualHealth, "The Knight's armor should reduce damage gotten by " + (int) getArmor.invoke(knightArthur));
