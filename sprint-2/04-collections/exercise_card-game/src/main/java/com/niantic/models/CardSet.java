e03dc2777c3f1d7953a810888fff6fff6ba7763f
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-23 16:22:13 -0700

    Day25: Card Game - create CardSet class to track sets collected by player
    - class represents set of four cards of the same rank (with the same face value)
    - can calculate Player's score by tracking the number of CardSets the player collected

---
+++ sprint-2/04-collections/exercise_card-game/src/main/java/com/niantic/models/CardSet.java
@@ -0,0 +1,25 @@
+package com.niantic.models;
+
+import com.niantic.models.enums.FaceValue;
+import com.niantic.models.enums.Suit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CardSet {
+    private final FaceValue setValue;
+    private final ArrayList<Card> cards;
+
+    public CardSet(FaceValue faceValue) {
+        this.setValue = faceValue;
+
+        Suit[] suits = {Suit.CLUBS, Suit.SPADES, Suit.HEARTS, Suit.DIAMONDS};
+        cards = new ArrayList<>();
+
+        for (var suit : suits) {
+            cards.add(new Card(suit, faceValue));
+        }
+
+        Collections.sort(cards);
+    }
+}
