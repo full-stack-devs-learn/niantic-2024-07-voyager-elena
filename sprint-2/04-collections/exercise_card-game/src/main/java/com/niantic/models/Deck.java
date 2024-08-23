89160b8eeefbce17f303c696f926ea4ad8aa5006
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-22 23:31:04 -0700

    Day24: Card Game - Deck class
    - 52 French-suited playing cards
    - getCardCount method
    - takeRandomCard method
    - shuffle method

--- sprint-2/04-collections/exercise_card-game/src/main/java/com/niantic/models/Deck.java
+++ sprint-2/04-collections/exercise_card-game/src/main/java/com/niantic/models/Deck.java
@@ -1,43 +1,55 @@
 package com.niantic.models;
 
+import com.niantic.models.enums.FaceValue;
+import com.niantic.models.enums.Suit;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Deck
-{
-    private ArrayList<Card> cards;
+public class Deck {
+    private final ArrayList<Card> cards;
 
-    public Deck()
-    {
-        // build the deck of cards
+    public Deck() {
+        // French-suited playing cards
         cards = new ArrayList<>();
 
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        String[] colors = {"Red", "Blue", "Green", "Yellow"};
+        FaceValue[] faces = {FaceValue.TWO,
+                FaceValue.THREE,
+                FaceValue.FOUR,
+                FaceValue.FIVE,
+                FaceValue.SIX,
+                FaceValue.SEVEN,
+                FaceValue.EIGHT,
+                FaceValue.NINE,
+                FaceValue.TEN,
+                FaceValue.JACK,
+                FaceValue.QUEEN,
+                FaceValue.KING,
+                FaceValue.ACE};
+        Suit[] suits = {Suit.CLUBS, Suit.SPADES, Suit.HEARTS, Suit.DIAMONDS};
 
-        for(String color : colors)
-        {
-            for(int number : numbers)
-            {
-                Card card = new Card(color,number);
+        for (var suit : suits) {
+            for (var face : faces) {
+                Card card = new Card(suit, face);
                 cards.add(card);
             }
         }
     }
 
-    public int getCardCount()
-    {
+    public int getCardCount() {
         return cards.size();
     }
 
-    public Card takeCard()
-    {
-        Card card = cards.removeFirst();
-        return card;
+    public Card takeRandomCard() {
+        int randIdx = (int) (Math.random() * cards.size());
+        System.out.println("Deck size: " + cards.size());
+        System.out.println("Random card index: " + randIdx);
+        Card randomCard = cards.remove(randIdx);
+        System.out.println(randomCard);
+        return randomCard;
     }
 
-    public void shuffle()
-    {
+    public void shuffle() {
         Collections.shuffle(cards);
     }
 }
