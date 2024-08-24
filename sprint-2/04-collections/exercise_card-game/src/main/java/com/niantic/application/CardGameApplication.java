743baf6db2e59b089a00fe48b4439d92db919864
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-24 16:59:46 -0700

    Day25: Card Game - start implementing the CardGameApplication class

--- sprint-2/04-collections/exercise_card-game/src/main/java/com/niantic/application/CardGameApplication.java
+++ sprint-2/04-collections/exercise_card-game/src/main/java/com/niantic/application/CardGameApplication.java
@@ -7,35 +7,63 @@ import com.niantic.ui.UserInterface;
 
 import java.util.ArrayList;
 
-public class CardGameApplication
-{
+public class CardGameApplication {
     Deck deck = new Deck();
     ArrayList<Player> players = new ArrayList<>();
-    
-    public void run()
-    {
-        addPlayers();
-        dealCards();
+
+    public void run() {
+        startGame();
+        gamePlay();
         UserInterface.displayAllPlayersCards(players);
+        UserInterface.printAllPlayersScores(players);
+        Player winner = determineWinner();
+        UserInterface.declareWinner(winner);
     }
 
-    private void dealCards()
-    {
-        deck.shuffle();
+    private Player determineWinner() {
+        return players.getFirst();
+    }
 
-        for (int i = 0; i < 2; i++)
-        {
-            for(Player player : players)
-            {
-                Card card = deck.takeCard();
+    private void gamePlay() {
+    }
+
+    private void startGame() {
+        // TODO:
+        //   play with computer or with other players
+        //   how many players?
+        //   enter players names
+        System.out.println("Deck before shuffling: ");
+        deck.displayAllCardsInDeck();
+        System.out.println("Number of cards in the deck: " + deck.getCardCount());
+        createPlayers();
+        dealCards();
+    }
+
+    private void createPlayers() {
+        // TODO:
+        //   ask user how many players (2 - 5)
+        //   ask to enter player names
+
+        // for now there will be only two players: Player 1 and Player 2
+        players.add(new Player("Player 1"));
+        players.add(new Player("Player 2"));
+    }
+
+    private void dealCards() {
+        deck.shuffle();
+        // five cards are dealt from a standard 52-card deck to each player,
+        // or seven cards if there are only two players
+        int numberOfCardsToStart = 5;
+        if (players.size() == 2) {
+            numberOfCardsToStart = 7;
+        }
+        for (int i = 0; i < numberOfCardsToStart; i++) {
+            for (Player player : players) {
+                Card card = deck.drawCard();
                 player.dealTo(card);
             }
         }
     }
 
-    private void addPlayers()
-    {
-        players.add(new Player("One"));
-        players.add(new Player("Two"));
-    }
+
 }
