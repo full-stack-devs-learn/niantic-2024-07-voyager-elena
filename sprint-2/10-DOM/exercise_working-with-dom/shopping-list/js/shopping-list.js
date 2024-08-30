39a8e1ab2ac91fc4409b7e9baee1d9a537810273
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-30 14:01:33 -0700

    Day30: Shopping List - implement displayGroceries

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -16,7 +16,39 @@ const displayListTitle = () => {
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() { }
+const displayGroceries = () => {
+    const shoppingListItemsContainer = document.querySelector('#groceries');
+    const shoppingList = shoppingListService.getShoppingList();
+    shoppingList.forEach(item => addShoppingListItemElement(item, shoppingListItemsContainer));
+}
+
+/**
+ * This function creates shopping list item element and appends it to the items container
+ */
+const addShoppingListItemElement = (item, container) => container.appendChild(createShoppingListItemElement(item));
+
+
+const createShoppingListItemElement = (item) => {
+    const itemElement = document.createElement('li');
+    itemElement.appendChild(createShoppingListItemTitleDiv(item.title));
+    itemElement.appendChild(createShoppingListItemQuantityDiv(item.quantity));
+    if (item.isComplete) {
+        itemElement.classList.add('complete');
+    }
+    return itemElement;
+}
+
+const createShoppingListItemTitleDiv = (title) => {
+    const titleElement = document.createElement('div');
+    titleElement.textContent = title;
+    return titleElement;
+}
+
+const createShoppingListItemQuantityDiv = (quantity) => {
+    const quantityElement = document.createElement('div');
+    quantityElement.textContent = quantity;
+    return quantityElement;
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
