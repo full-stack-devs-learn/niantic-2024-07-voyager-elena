94ad5c50f9bef929858bb0bc57e1bfbd6052507a
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-04 15:10:24 -0700

    Day32: SpringBoot with JS - fetch the data from the new GetMapping

--- sprint-2/12_javascript-and-thymeleaf/exercise_javascript-and-thymeleaf/src/main/resources/static/js/products-list.js
+++ sprint-2/12_javascript-and-thymeleaf/exercise_javascript-and-thymeleaf/src/main/resources/static/js/products-list.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     const categorySelector = document.querySelector('#category');
-    categorySelector.addEventListener('change', () => console.log(`You selected category with id: ${categorySelector.value}`))
-
+    categorySelector.addEventListener('change', () => loadProducts(categorySelector.value));
 });
+
+const loadProducts = (categoryId) => {
+    const container = document.querySelector('#product-container');
+    container.innerHTML = '';
+
+    const url = `/products/category/${categoryId}`;
+
+    fetch(url).then(response => {
+        if (response.status === 200) {
+            return response.text();
+        }
+        throw new Error(response);
+    }).then(data => {
+        container.innerHTML = data;
+    }).catch(error => {
+        console.log(error);
+    });
+};
