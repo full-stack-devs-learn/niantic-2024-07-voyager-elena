0e700c057158f1c700cdaaf9579bc9fb641437bd
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-05 01:49:54 -0700

    Day32: SpringBoot with JS - create ProductApiController

---
+++ sprint-2/12_javascript-and-thymeleaf/exercise_javascript-and-thymeleaf/src/main/java/com/niantic/controllers/apis/ProductApiController.java
@@ -0,0 +1,18 @@
+package com.niantic.controllers.apis;
+
+import com.niantic.models.Product;
+import com.niantic.services.ProductDao;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class ProductApiController {
+    @GetMapping("/products/category/{categoryId}")
+    public ArrayList<Product> getProductsByCategoryId(@PathVariable int categoryId) {
+        ProductDao productDao = new ProductDao();
+        return productDao.getProductsByCategory(categoryId);
+    }
+}
