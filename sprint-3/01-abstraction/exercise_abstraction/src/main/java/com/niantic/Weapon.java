70eabb24343d8ce09d2e76a0d5ddde6f1113bb5c
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-11 13:30:49 -0700

    Abstraction - create abstract Weapon class
    - all Weapon tests passed

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Weapon.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Weapon.java
@@ -1,5 +1,34 @@
 package com.niantic;
 
-public class Weapon
-{
+public abstract class Weapon {
+    protected String name;
+    protected int damage;
+    protected int percentCharged;
+    protected int range;
+
+    public Weapon(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+        percentCharged = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getPercentCharged() {
+        return percentCharged;
+    }
+
+    public abstract int getRange();
+
+    protected abstract void setPercentCharged();
+
+    public abstract int attack();
+
+    public abstract int powerAttack();
 }
