1229cb4c25491f6f250cedaa53bcb3e67c5204d4
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-11 15:29:46 -0700

    Abstraction - create the Dagger class

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Dagger.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Dagger.java
@@ -1,5 +1,59 @@
 package com.niantic;
 
-public class Dagger
-{
+public class Dagger extends Weapon {
+    private int daggerCount;
+
+    public Dagger(String name, int damage, int daggerCount) {
+        super(name, damage);
+        this.daggerCount = daggerCount;
+    }
+
+    public int getDaggerCount() {
+        return daggerCount;
+    }
+
+    @Override
+    public int attack() {
+        // If there are daggers in the bag the attack delivers the default damage to the other character
+        if (daggerCount == 0) {
+            return 0;
+        }
+
+        // With a regular attack, daggers are always retrieved - so the count does not go down
+        // Each throw charges the percentCharged by 20%
+        int percentCharged = getPercentCharged();
+        percentCharged += 20;
+        // the Percent charged cannot exceed 100%
+        if (percentCharged > 100) {
+            percentCharged = 100;
+        }
+        setPercentCharged(percentCharged);
+
+        return getDamage();
+    }
+
+    @Override
+    public int powerAttack() {
+        int percentCharged = getPercentCharged();
+
+        // can only be used if the dagger is charged 100%
+        if (percentCharged < 100 || daggerCount == 0) {
+            return 0;
+        }
+
+        // A dagger cannot be retrieved from a power attack, so the daggerCount is reduced by 1
+        daggerCount--;
+        setPercentCharged(0);
+        // 3x the default
+        return 3 * getDamage();
+    }
+
+    @Override
+    public int getRange() {
+        return 10;
+    }
+
+    public void addDagger() {
+        daggerCount++;
+    }
 }
