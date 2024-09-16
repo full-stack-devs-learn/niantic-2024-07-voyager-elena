b0ff642b1470cd87cab0d856e59a182f948a720a
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-16 13:33:53 -0700

    File IO reading: move getting files into a separate method, getAllFiles

--- sprint-3/04-file-io/exercises_file-io-exceptions/src/main/java/com/niantic/application/GradingApplication.java
+++ sprint-3/04-file-io/exercises_file-io-exceptions/src/main/java/com/niantic/application/GradingApplication.java
@@ -8,17 +8,13 @@ import com.niantic.ui.UserInput;
 import java.io.File;
 import java.util.Arrays;
 
-public class GradingApplication implements Runnable
-{
+public class GradingApplication implements Runnable {
     private GradesService gradesService = new GradesFileService();
 
-    public void run()
-    {
-        while(true)
-        {
+    public void run() {
+        while (true) {
             int choice = UserInput.homeScreenSelection();
-            switch(choice)
-            {
+            switch (choice) {
                 case 1:
                     displayAllFiles();
                     break;
@@ -43,48 +39,54 @@ public class GradingApplication implements Runnable
         }
     }
 
-    private void displayAllFiles()
-    {
-        // todo: 1 - get and display all student file names
+    private String[] getAllFiles() {
         File directory = new File("files");
         String[] files = directory.list();
         Arrays.sort(files);
+
+        return files;
+    }
+
+    private void displayAllFiles() {
+        // todo: 1 - get and display all student file names
+
+        String[] files = getAllFiles();
+
+        System.out.println();
+        System.out.println(" All students files");
+        System.out.println("=".repeat(20));
 
         for (var fileName : files) {
             System.out.println(fileName);
         }
     }
 
-    private void displayFileScores()
-    {
+    private void displayFileScores() {
         // todo: 2 - allow the user to select a file name
         // load all student assignment scores from the file - display all files
+
     }
 
-    private void displayStudentAverages()
-    {
+    private void displayStudentAverages() {
         // todo: 3 - allow the user to select a file name
         // load all student assignment scores from the file - display student statistics (low score, high score, average score)
 
     }
 
-    private void displayAllStudentStatistics()
-    {
+    private void displayAllStudentStatistics() {
         // todo: 4 - Optional / Challenge - load all scores from all student and all assignments
         // display the statistics for all scores (low score, high score, average score, number of students, number of assignments)
     }
 
-    private void displayAssignmentStatistics()
-    {
+    private void displayAssignmentStatistics() {
         // todo: 5 - Optional / Challenge - load all scores from all student and all assignments
         // display the statistics for each assignment (assignment name, low score, high score, average score)
         // this one could take some time
     }
 
-    private String parseStudentName(String fileName)
-    {
+    private String parseStudentName(String fileName) {
         return fileName.replace(".csv", "")
-                        .replace("_", " ")
-                        .substring(10);
+                .replace("_", " ")
+                .substring(10);
     }
 }
