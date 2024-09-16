186ae077bcec90e52610d021e1351b3994da9fdd
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-16 01:45:34 -0700

    Abstraction: add tests for attack and powerAttack

--- sprint-3/01-abstraction/exercise_abstraction/src/test/java/com/niantic/BowTest.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/test/java/com/niantic/BowTest.java
@@ -2,7 +2,7 @@ package com.niantic;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.function.BooleanSupplier;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -82,9 +82,89 @@ class BowTest {
         String expectedMessage = "Invalid arrow type: Ice";
         Exception exception = assertThrows(IllegalArgumentException.class, () -> new Bow(bowName, damage, "Ice", quiverSize),
                 "Should throw IllegalArgumentException when arrowType is not standard, poison or explosive"
-                );
+        );
         assertEquals(expectedMessage, exception.getMessage());
     }
 
+    @Test
+    void getRange_shouldReturn20() {
+        // arrange
+        bow = new Bow(bowName, damage, standardType, quiverSize);
+        int expectedRange = 20;
+
+        // act
+        int actualRange = bow.getRange();
+
+        // assert
+        assertEquals(expectedRange, actualRange, "The bow range should be equal to 20");
+    }
+
+    @Test
+    void attack_shouldReturn_defaultDamage_forStandardArrows() {
+        // arrange
+        bow = new Bow(bowName, damage, standardType, quiverSize);
+        int expectedAttackResult = damage;
+
+        // act
+        int actualAttackResult = bow.attack();
+
+        // assert
+        assertEquals(expectedAttackResult, actualAttackResult, "Attack with " + standardType + " arrow should return default damage");
+    }
+
+    @Test
+    void attack_shouldReturn_2xDamage_forPoisonArrows() {
+        // arrange
+        bow = new Bow(bowName, damage, poisonType, quiverSize);
+        int expectedAttackResult = 2 * damage;
+
+        // act
+        int actualAttackResult = bow.attack();
+
+        // assert
+        assertEquals(expectedAttackResult, actualAttackResult, "Attack with " + poisonType + " arrow should return default damage");
+    }
+
+    @Test
+    void attack_shouldReturn_3xDamage_forExplosiveArrows() {
+        // arrange
+        bow = new Bow(bowName, damage, explosiveType, quiverSize);
+        int expectedAttackResult = 3 * damage;
+
+        // act
+        int actualAttackResult = bow.attack();
+
+        // assert
+        assertEquals(expectedAttackResult, actualAttackResult, "Attack with " + explosiveType + " arrow should return default damage");
+    }
+
+    @Test
+    void powerAttack_performsRegularAttack_whenPercentChargedBelow100() throws InterruptedException {
+        // arrange
+        bow = new Bow(bowName, damage, standardType, quiverSize);
+        int expectedPowerAttackResult = damage;
+
+        // act
+        int actualPowerAttackResult = bow.powerAttack();
+
+        // assert
+        assertEquals(expectedPowerAttackResult, actualPowerAttackResult, "Power attack should return default damage if percentCharged < 100%");
+    }
+
+    @Test
+    void powerAttack_performsDeliversDoubleAttackPower_whenPercentChargedIs100() throws InterruptedException {
+        // arrange
+        bow = new Bow(bowName, damage, standardType, quiverSize);
+        int expectedPowerAttackResult = 2 * damage;
+
+
+        // act
+        // wait 2 * 5 seconds to charge bow
+        TimeUnit.SECONDS.sleep(10);
+        int actualPowerAttackResult = bow.powerAttack();
+
+        // assert
+        assertEquals(expectedPowerAttackResult, actualPowerAttackResult, "Power attack should deliver double attack power when percentCharged = 100%");
+    }
 
 }
