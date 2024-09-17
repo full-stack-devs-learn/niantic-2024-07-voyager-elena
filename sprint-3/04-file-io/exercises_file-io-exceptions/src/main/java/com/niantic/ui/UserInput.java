9201a11339ebda65a13d0ee7bf088ed26ceaaae0
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-16 23:12:35 -0700

    File IO reading:  add user input validation for the main menu

--- sprint-3/04-file-io/exercises_file-io-exceptions/src/main/java/com/niantic/ui/UserInput.java
+++ sprint-3/04-file-io/exercises_file-io-exceptions/src/main/java/com/niantic/ui/UserInput.java
@@ -6,26 +6,44 @@ public class UserInput {
     protected static Scanner in = new Scanner(System.in);
 
     public static int homeScreenSelection() {
-        System.out.println();
-        System.out.println("What do you want to do?");
-        System.out.println("-".repeat(30));
-        System.out.println();
-        System.out.println("  1) Display files");
-        System.out.println();
-        System.out.println("  ------------ Individual File ------------");
-        System.out.println("  2) Student: display all scores");
-        System.out.println("  3) Student: display average score");
-        System.out.println();
-        System.out.println("  ---------- Challenge All Files ----------");
-        System.out.println("  5) All Students: display average score");
-        System.out.println("  6) All Assignments: display average score");
-        System.out.println();
-        System.out.println("  0) Exit");
+        boolean valid = false;
+        int choice = -1;
 
-        System.out.println();
-        System.out.print("Please make a selection: ");
+        while (!valid) {
+            System.out.println();
+            System.out.println("What do you want to do?");
+            System.out.println("-".repeat(30));
+            System.out.println();
+            System.out.println("  1) Display files");
+            System.out.println();
+            System.out.println("  ------------ Individual File ------------");
+            System.out.println("  2) Student: display all scores");
+            System.out.println("  3) Student: display average score");
+            System.out.println();
+            System.out.println("  ---------- Challenge All Files ----------");
+            System.out.println("  5) All Students: display average score");
+            System.out.println("  6) All Assignments: display average score");
+            System.out.println();
+            System.out.println("  0) Exit");
 
-        return Integer.parseInt(in.nextLine());
+            System.out.println();
+            System.out.print("Please make a selection: ");
+
+            String choiceStr = in.nextLine();
+
+            try {
+                choice = Integer.parseInt(choiceStr);
+            } catch (Exception e) {
+                choice = -1;
+            }
+            if (choice < 0 || choice > 6) {
+                displayMessage("Invalid input, please make a valid selection");
+            } else {
+                valid = true;
+            }
+        }
+
+        return choice;
     }
 
     public static int displayMenuToChooseFile(String[] files) {
