deae9e163840ffc1da7fc23a851983ba5e7007fe
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-19 15:34:15 -0700

    REST API: Challenge - Add Dependency Injection to the project

--- sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/CategoriesController.java
+++ sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/CategoriesController.java
@@ -2,47 +2,47 @@ package com.niantic.controllers;
 
 import com.niantic.models.Category;
 import com.niantic.services.CategoryDao;
-import com.niantic.services.MySqlCategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-public class CategoriesController
-{
-    private CategoryDao categoryDao = new MySqlCategoryDao();
+@RequestMapping("/api/categories")
+public class CategoriesController {
+    private final CategoryDao categoryDao;
 
-    @GetMapping("/api/categories")
-    public List<Category> getAllCategories()
-    {
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
+
+    @GetMapping("")
+    public List<Category> getAllCategories() {
         return categoryDao.getCategories();
     }
 
-    @GetMapping("/api/categories/{id}")
-    public Category getCategory(@PathVariable int id)
-    {
+    @GetMapping("{id}")
+    public Category getCategory(@PathVariable int id) {
         return categoryDao.getCategory(id);
     }
 
-    @PostMapping("/api/categories")
+    @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public Category addCategory(@RequestBody Category category)
-    {
+    public Category addCategory(@RequestBody Category category) {
         return categoryDao.addCategory(category);
     }
 
-    @PutMapping("/api/categories/{id}")
+    @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateCategory(@PathVariable int id, @RequestBody Category category)
-    {
+    public void updateCategory(@PathVariable int id, @RequestBody Category category) {
         categoryDao.updateCategory(id, category);
     }
 
-    @DeleteMapping("/api/categories/{id}")
+    @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteCategory(@PathVariable int id)
-    {
+    public void deleteCategory(@PathVariable int id) {
         categoryDao.deleteCategory(id);
     }
 
--- sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/services/MySqlCategoryDao.java
+++ sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/services/MySqlCategoryDao.java
@@ -1,11 +1,12 @@
 package com.niantic.services;
 
 import com.niantic.models.Category;
-import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.PreparedStatement;
@@ -13,37 +14,26 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MySqlCategoryDao implements CategoryDao
-{
+@Repository
+public class MySqlCategoryDao implements CategoryDao {
     private final JdbcTemplate jdbcTemplate;
 
-    public MySqlCategoryDao()
-    {
-        String databaseUrl = "jdbc:mysql://localhost:3306/northwind";
-        String userName = "root";
-        String password = "P@ssw0rd";
-        DataSource dataSource = new BasicDataSource(){{
-            setUrl(databaseUrl);
-            setUsername(userName);
-            setPassword(password);
-        }};
-
+    @Autowired
+    public MySqlCategoryDao(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public List<Category> getCategories()
-    {
+    public List<Category> getCategories() {
         List<Category> categories = new ArrayList<>();
 
         String sql = """
-        SELECT category_id, category_name, description
-        FROM categories
-        """;
+                SELECT category_id, category_name, description
+                FROM categories
+                """;
 
         SqlRowSet row = jdbcTemplate.queryForRowSet(sql);
 
-        while(row.next())
-        {
+        while (row.next()) {
             int categoryId = row.getInt("category_id");
             String categoryName = row.getString("category_name");
             String description = row.getString("description");
@@ -56,20 +46,18 @@ public class MySqlCategoryDao implements CategoryDao
         return categories;
     }
 
-    public Category getCategory(int categoryId)
-    {
+    public Category getCategory(int categoryId) {
         Category category = null;
 
         String sql = """
-                SELECT category_id, category_name, description
-                FROM categories
-                WHERE category_id = ?
-        """;
+                        SELECT category_id, category_name, description
+                        FROM categories
+                        WHERE category_id = ?
+                """;
 
         SqlRowSet row = jdbcTemplate.queryForRowSet(sql, categoryId);
 
-        if(row.next())
-        {
+        if (row.next()) {
             String categoryName = row.getString("category_name");
             String description = row.getString("description");
 
@@ -80,8 +68,7 @@ public class MySqlCategoryDao implements CategoryDao
         return category;
     }
 
-    public Category addCategory(Category category)
-    {
+    public Category addCategory(Category category) {
         String sql = """
                 INSERT INTO categories (category_name, description)
                 VALUES (?,?)
@@ -105,9 +92,7 @@ public class MySqlCategoryDao implements CategoryDao
         return getCategory(newId);
     }
 
-
-    public void updateCategory(int id, Category category)
-    {
+    public void updateCategory(int id, Category category) {
         String sql = """
                 UPDATE categories
                 SET category_name = ?
@@ -121,9 +106,7 @@ public class MySqlCategoryDao implements CategoryDao
                 , id);
     }
 
-
-    public void deleteCategory(int categoryId)
-    {
+    public void deleteCategory(int categoryId) {
         String sql = """
                 DELETE FROM categories
                 WHERE category_id = ?
@@ -131,6 +114,5 @@ public class MySqlCategoryDao implements CategoryDao
 
         jdbcTemplate.update(sql, categoryId);
     }
-
 
 }
