f3fc87fac04701289553972ce285eb04e0cac80e
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-20 14:17:35 -0700

    API Review: add CrossOrigin to controllers, change base url

--- sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/CategoriesController.java
+++ sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/CategoriesController.java
@@ -8,9 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
+@CrossOrigin
 @RequestMapping("/api/categories")
 public class CategoriesController {
     private final CategoryDao categoryDao;
