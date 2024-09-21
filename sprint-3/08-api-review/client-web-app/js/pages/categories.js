9cd8589e7793df6b9493ea7bdbdb02237862d7a1
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-20 18:55:11 -0700

    API Review: add edit category form

--- sprint-3/08-api-review/client-web-app/js/pages/categories.js
+++ sprint-3/08-api-review/client-web-app/js/pages/categories.js
@@ -1,21 +1,26 @@
 let categoryService;
 let addFormScreen;
 let addForm;
+let editFormScreen;
+let editForm;
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     categoryService = new CategoryService();
     addFormScreen = document.getElementById("add-form-screen");
     addForm = document.getElementById("add-form");
+    editFormScreen = document.getElementById("edit-form-screen");
+    editForm = document.getElementById("edit-form");
 
-    document.getElementById("add-button").addEventListener("click", showForm);
+    document.getElementById("add-button").addEventListener("click", showAddForm);
     document.getElementById("cancel-button").addEventListener("click", cancelAdd);
     document.getElementById("save-button").addEventListener("click", addCategory);
+    document.getElementById("cancel-edit-button").addEventListener("click", cancelEdit);
+    document.getElementById("save-edit-button").addEventListener("click", updateCategory);
 
     loadCategories();
 })
 
-function loadCategories()
-{
+function loadCategories() {
     categoryService.getAllCategories()
         .then(categories => {
             const categoryContainer = document.getElementById('categories-container');
@@ -26,11 +31,15 @@ function loadCategories()
                 template.getElementById('category-header').innerText = category.categoryName;
                 template.getElementById('category-image').src = `images/${category.categoryId}.webp`;
 
-                const deleteButton = template.querySelector('.card-footer #delete-button');
+                const deleteButton = template.querySelector('#delete-button');
                 deleteButton.addEventListener('click', () => {
                     categoryService.deleteCategory(category.categoryId).then(() => {
                         loadCategories();
                     })
+                });
+                const editButton = template.querySelector('#edit-button');
+                editButton.addEventListener('click', () => {
+                    editCategory(category.categoryId);
                 });
 
                 categoryContainer.appendChild(template);
@@ -38,28 +47,31 @@ function loadCategories()
         });
 }
 
-function showForm()
-{
+function showAddForm() {
     addFormScreen.classList.remove("d-none");
 }
 
-function cancelAdd(event)
-{
+function cancelAdd(event) {
     event.preventDefault();
     addFormScreen.classList.add("d-none");
     addForm.reset();
 }
 
-function addCategory(event)
-{
+function cancelEdit(event) {
+    event.preventDefault();
+    editFormScreen.classList.add("d-none");
+    editForm.reset();
+}
+
+function addCategory(event) {
     event.preventDefault();
     event.stopPropagation()
 
     addForm.classList.add("was-validated");
 
-    if(addForm.checkValidity()){
-        const name = document.getElementById("category-name").value;
-        const description = document.getElementById("category-description").value;
+    if (addForm.checkValidity()) {
+        const name = addForm.querySelector("#category-name").value;
+        const description = addForm.querySelector("#category-description").value;
 
         const category = {
             categoryName: name,
@@ -75,3 +87,37 @@ function addCategory(event)
         addForm.classList.remove("was-validated");
     }
 }
+
+function editCategory(categoryId) {
+    console.log('Edit category');
+    console.log(categoryId);
+    //TODO: fill the form field with the category data
+    editFormScreen.classList.remove("d-none");
+}
+
+function updateCategory(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    editForm.classList.add("was-validated");
+
+    if (editForm.checkValidity()) {
+        const name = editForm.querySelector("#category-name").value;
+        const description = editForm.querySelector("#category-description").value;
+
+
+        const category = {
+            categoryName: name,
+            description: description,
+        }
+
+        categoryService.editCategory(category).then(response => {
+            editForm.reset();
+            loadCategories();
+        })
+
+        editFormScreen.classList.add("d-none");
+        editForm.classList.remove("was-validated");
+    }
+}
+
