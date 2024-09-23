00dcdf2a5f71045562f81ed375ffa81d14311570
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-22 23:10:21 -0700

    API Review: create ProductService

--- sprint-3/08-api-review/client-web-app/js/services/products-service.js
+++ sprint-3/08-api-review/client-web-app/js/services/products-service.js
@@ -1,5 +1,40 @@
 class ProductsService {
 
-    baseUrl = `${config.baseUrl}/products`;
+  baseUrl = `${config.baseUrl}/products`;
 
+  getAllProducts() {
+    return axios.get(this.baseUrl)
+      .then(response => {
+        return response.data;
+      });
+  }
+
+  getProductsByCategory(categoryId) {
+    return axios.get(`this.baseUrl?catId=${categoryId}`)
+      .then(response => {
+        return response.data;
+      });
+  }
+
+  getProductById(productId) {
+    return axios.get(`${this.baseUrl}/${productId}`)
+      .then(response => {
+        return response.data;
+      });
+  }
+
+  addProduct(product) {
+    return axios.post(this.baseUrl, product)
+      .then(response => {
+        return response.data;
+      });
+  }
+
+  updateProduct(productId, product) {
+    return axios.put(`${this.baseUrl}/${productId}`, product);
+  }
+
+  deleteCategory(productId) {
+    return axios.delete(`${this.baseUrl}/${productId}`);
+  }
 }
