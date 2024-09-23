c0ca805c90ef3e1253e40ad3111d96cec07d0b75
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-22 22:37:16 -0700

    API Review: implement editing category functionality

--- sprint-3/08-api-review/client-web-app/js/pages/categories.js
+++ sprint-3/08-api-review/client-web-app/js/pages/categories.js
@@ -4,120 +4,131 @@ let addForm;
 let editFormScreen;
 let editForm;
 
+let currentCategoryId;
+
 document.addEventListener("DOMContentLoaded", function () {
-    categoryService = new CategoryService();
-    addFormScreen = document.getElementById("add-form-screen");
-    addForm = document.getElementById("add-form");
-    editFormScreen = document.getElementById("edit-form-screen");
-    editForm = document.getElementById("edit-form");
+  categoryService = new CategoryService();
+  addFormScreen = document.getElementById("add-form-screen");
+  addForm = document.getElementById("add-form");
+  editFormScreen = document.getElementById("edit-form-screen");
+  editForm = document.getElementById("edit-form");
 
-    document.getElementById("add-button").addEventListener("click", showAddForm);
-    document.getElementById("cancel-button").addEventListener("click", cancelAdd);
-    document.getElementById("save-button").addEventListener("click", addCategory);
-    document.getElementById("cancel-edit-button").addEventListener("click", cancelEdit);
-    document.getElementById("save-edit-button").addEventListener("click", updateCategory);
+  document.getElementById("add-button").addEventListener("click", showAddForm);
+  document.getElementById("cancel-button").addEventListener("click", cancelAdd);
+  document.getElementById("save-button").addEventListener("click", addCategory);
+  document.getElementById("cancel-edit-button").addEventListener("click", cancelEdit);
+  document.getElementById("save-edit-button").addEventListener("click", updateCategory);
 
-    loadCategories();
+  loadCategories();
 })
 
 function loadCategories() {
-    categoryService.getAllCategories()
-        .then(categories => {
-            const categoryContainer = document.getElementById('categories-container');
-            categoryContainer.innerHTML = '';
+  categoryService.getAllCategories()
+    .then(categories => {
+      const categoryContainer = document.getElementById('categories-container');
+      categoryContainer.innerHTML = '';
 
-            categories.forEach(category => {
-                const template = document.getElementById('category-template').content.cloneNode(true);
-                template.getElementById('category-header').innerText = category.categoryName;
-                template.getElementById('category-image').src = `images/${category.categoryId}.webp`;
+      categories.forEach(category => {
+        const categoryCard = document.getElementById('category-template').content.cloneNode(true);
+        categoryCard.getElementById('category-header').innerText = category.categoryName;
+        if (category.categoryId < 9) {
+          categoryCard.getElementById('category-image').src = `images/${category.categoryId}.webp`;
+        }
 
-                const deleteButton = template.querySelector('#delete-button');
-                deleteButton.addEventListener('click', () => {
-                    categoryService.deleteCategory(category.categoryId).then(() => {
-                        loadCategories();
-                    })
-                });
-                const editButton = template.querySelector('#edit-button');
-                editButton.addEventListener('click', () => {
-                    editCategory(category.categoryId);
-                });
-
-                categoryContainer.appendChild(template);
-            })
+        const deleteButton = categoryCard.querySelector('#delete-button');
+        deleteButton.addEventListener('click', () => {
+          categoryService.deleteCategory(category.categoryId).then(() => {
+            loadCategories();
+          })
         });
+        const editButton = categoryCard.querySelector('#edit-button');
+        editButton.addEventListener('click', () => {
+          editCategory(category.categoryId);
+        });
+
+        categoryContainer.appendChild(categoryCard);
+      })
+    });
 }
 
 function showAddForm() {
-    addFormScreen.classList.remove("d-none");
+  addFormScreen.classList.remove("d-none");
 }
 
 function cancelAdd(event) {
-    event.preventDefault();
-    addFormScreen.classList.add("d-none");
-    addForm.reset();
+  event.preventDefault();
+  addFormScreen.classList.add("d-none");
+  addForm.reset();
 }
 
 function cancelEdit(event) {
-    event.preventDefault();
-    editFormScreen.classList.add("d-none");
-    editForm.reset();
+  event.preventDefault();
+  editFormScreen.classList.add("d-none");
+  editForm.reset();
 }
 
 function addCategory(event) {
-    event.preventDefault();
-    event.stopPropagation()
+  event.preventDefault();
+  event.stopPropagation()
 
-    addForm.classList.add("was-validated");
+  addForm.classList.add("was-validated");
 
-    if (addForm.checkValidity()) {
-        const name = addForm.querySelector("#category-name").value;
-        const description = addForm.querySelector("#category-description").value;
+  if (addForm.checkValidity()) {
+    const name = addForm.querySelector("#category-name").value;
+    const description = addForm.querySelector("#category-description").value;
 
-        const category = {
-            categoryName: name,
-            description: description,
-        }
-
-        categoryService.addCategory(category).then(response => {
-            addForm.reset();
-            loadCategories();
-        })
-
-        addFormScreen.classList.add("d-none");
-        addForm.classList.remove("was-validated");
+    const category = {
+      categoryName: name,
+      description: description,
     }
+
+    categoryService.addCategory(category).then(response => {
+      addForm.reset();
+      loadCategories();
+    })
+
+    addFormScreen.classList.add("d-none");
+    addForm.classList.remove("was-validated");
+  }
 }
 
 function editCategory(categoryId) {
-    console.log('Edit category');
-    console.log(categoryId);
-    //TODO: fill the form field with the category data
-    editFormScreen.classList.remove("d-none");
+  console.log('Edit category');
+  console.log(categoryId);
+  // fill the form fields with the category data
+  categoryService.getCategoryById(categoryId).then(category => {
+    const name = editForm.querySelector("#category-name");
+    const description = editForm.querySelector("#category-description");
+    name.value = category.categoryName;
+    description.value = category.description;
+    currentCategoryId = category.categoryId;
+  });
+  editFormScreen.classList.remove("d-none");
 }
 
 function updateCategory(event) {
-    event.preventDefault();
-    event.stopPropagation();
+  event.preventDefault();
+  event.stopPropagation();
 
-    editForm.classList.add("was-validated");
+  editForm.classList.add("was-validated");
 
-    if (editForm.checkValidity()) {
-        const name = editForm.querySelector("#category-name").value;
-        const description = editForm.querySelector("#category-description").value;
+  if (editForm.checkValidity()) {
+    const name = editForm.querySelector("#category-name").value;
+    const description = editForm.querySelector("#category-description").value;
 
 
-        const category = {
-            categoryName: name,
-            description: description,
-        }
-
-        categoryService.editCategory(category).then(response => {
-            editForm.reset();
-            loadCategories();
-        })
-
-        editFormScreen.classList.add("d-none");
-        editForm.classList.remove("was-validated");
+    const category = {
+      categoryName: name,
+      description: description,
     }
+
+    categoryService.updateCategory(currentCategoryId, category).then(response => {
+      editForm.reset();
+      loadCategories();
+    })
+
+    editFormScreen.classList.add("d-none");
+    editForm.classList.remove("was-validated");
+  }
 }
 
