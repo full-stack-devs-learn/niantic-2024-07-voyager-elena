cdc6fe22fede8e93ef96e8972b0889ff7b8ca42a
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-30 21:44:15 -0700

    React Router: install bootstrap, react-bootstrap, and bootswatch

--- sprint-3/14-react-routing/exercise_react-routing/northwind-react-ts-routing/src/main.tsx
+++ sprint-3/14-react-routing/exercise_react-routing/northwind-react-ts-routing/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootswatch/dist/united/bootstrap.min.css'
+
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
