fd35ff87587ed1c89f13dc756e17af822374a2f6
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-10-01 21:34:21 -0700

    React Router: get and display search parameters in ProductSearch

--- sprint-3/14-react-routing/exercise_react-routing/northwind-react-ts-routing/src/components/products/product-search/ProductSearch.tsx
+++ sprint-3/14-react-routing/exercise_react-routing/northwind-react-ts-routing/src/components/products/product-search/ProductSearch.tsx
@@ -1,10 +1,23 @@
+import { useLocation } from 'react-router-dom'
 import './ProductSearch.css'
 
 const ProductSearch = () => {
 
+  const location = useLocation()
+  const params = new URLSearchParams(location.search)
+  const minPrice = params.get('minPrice')
+  const maxPrice = params.get('maxPrice')
+  const name = params.get('name')
+  const catId = params.get('catId')
+
   return (
     <>
       <h3 className="mb-3">Search Product</h3>
+      <div>Search parameters:</div>
+      <div>minPrice: {minPrice}</div>
+      <div>maxPrice: {maxPrice}</div>
+      <div>name: {name}</div>
+      <div>catId: {catId}</div>
     </>
   )
 }
