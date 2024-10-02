fc26ec62bf281b97ac3a4f1f2fdb54b11463aa73
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-10-01 23:59:29 -0700

    React Router: create category model and category service

---
+++ sprint-3/14-react-routing/exercise_react-routing/northwind-react-ts-routing/src/models/category.ts
@@ -0,0 +1,5 @@
+export default interface Category {
+  categoryId: number
+  categoryName: string
+  description: string
+}
---
+++ sprint-3/14-react-routing/exercise_react-routing/northwind-react-ts-routing/src/services/category-service.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import Category from '../models/category'
+
+/*
+* I used my Northwind API server from day 06 exercise
+* https://github.com/full-stack-devs-learn/niantic-2024-07-voyager-elena/tree/main/sprint-3/06-rest-api/exercise_rest-api
+*/
+
+class CategoryService {
+  baseUrl = 'http://localhost:8080/api/categories'
+
+  async getAllCategories() {
+    const response = await axios.get(this.baseUrl)
+    return response.data
+  }
+
+  async getCategoryById(categoryId: number) {
+    const response = await axios.get(`${this.baseUrl}/${categoryId}`)
+    return response.data
+  }
+
+  async addCategory(category: Category) {
+    const response = await axios.post(this.baseUrl, category)
+    return response.data
+  }
+
+  async updateCategory(category: Category) {
+    const response = await axios.put(`${this.baseUrl}/${category.categoryId}`, category)
+    return response.data
+  }
+
+  async deleteCategory(categoryId: number) {
+    await axios.delete(`${this.baseUrl}/${categoryId}`)
+  }
+}
+
+const categoryService = new CategoryService()
+export default categoryService
